4fce1f9a28c018e9a9c6d9bba3c9a29f59cb1128
Siddhesh Mangela <sidthecool007@gmail.com>
2016-09-12 11:40:54 +0530

    About paragraph added

--- theft/scripts/main.js
+++ theft/scripts/main.js
@@ -126,4 +126,23 @@ document.onkeydown = function(e) {
     }
 };
 
+// About
+(function aboutInfo() {
+    var about = document.querySelector('#about'),
+        close = document.querySelector('#aboutClose');
+
+    about.addEventListener('click', function () {
+       var parentDom = this.parentElement,
+           aboutPara = parentDom.querySelector('p');
         
+        aboutPara.classList.add('show');
+        this.classList.add('close');
+    });
+
+    close.addEventListener('click', function () {
+       var aboutPara = this.parentElement.parentElement;
+        
+        aboutPara.classList.remove('show');
+        about.classList.remove('close');
+    });
+})();
