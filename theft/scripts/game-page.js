b9c8f3f8789401f9a354823d994dd64cd21758b3
Siddhesh Mangela <sidthecool007@gmail.com>
2016-09-20 10:43:44 +0530

    game info close button fixed for tab and mobile

--- theft/scripts/game-page.js
+++ theft/scripts/game-page.js
@@ -31,41 +31,29 @@
             allSubHolders[j].classList.add('expand');
         }
     });
-    // Game Info
-    (function gameInfo() {
-        var gameInfo = document.querySelector('#gamesInfo');
-
-        gameInfo.addEventListener('click', function () {
-            var parentDom = this.parentElement,
-                aboutPara = parentDom.querySelector('#page-descHolder');
-        
-                aboutPara.classList.add('show');
-                this.classList.add('close');
-        });
-
-       /* gameInfoClose.addEventListener('click', function () {
-            var aboutPara = this.parentElement.parentElement;
-        
-            aboutPara.classList.remove('show');
-            gameInfo.classList.remove('close');
-        });*/
-    })();
-
-    // Game Info alt
-    (function gameInfoAlt() {
-           var gameInfo = document.querySelector('#gamesInfoAlt'),
-               gameDesc = document.querySelector('.gameDesc'),
-               aboutPara = gameDesc.querySelector('#page-descHolder');
-
-           gameInfo.addEventListener('click', function () {
-                   aboutPara.classList.toggle('show');
-                   this.classList.toggle('close');
-           });
-
-           /*gameInfoClose.addEventListener('click', function () {
-               aboutPara.classList.remove('show');
-               gameInfo.classList.remove('close');
-           });*/
-       })();
-
 })();
+
+// game info expand
+(function gameInfo() {
+    var gameInfo = document.querySelector('#gamesInfo'),
+        gameInfoAlt = document.querySelector('#gamesInfoAlt'),
+        aboutPara = document.querySelector('#page-descHolder'),
+        gameInfoClose = document.querySelector('#gamesInfoClose');
+
+    gameInfo.addEventListener('click', function () {
+            aboutPara.classList.add('show');
+            this.classList.add('close');
+    });
+
+    gameInfoClose.addEventListener('click', function () {
+        aboutPara.classList.remove('show');
+        gameInfo.classList.remove('close');
+        gameInfoAlt.classList.remove('close');
+    });
+    
+    gameInfoAlt.addEventListener('click', function () {
+        gameInfo.click();
+    });
+    
+})();
+
